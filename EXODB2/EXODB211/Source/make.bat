117db643d0d1862bb79bab2552f81bb9a4120c35
Michael Johne <mjohne@users.noreply.github.com>
2018-11-14 16:45:35 +0100

    Add files via upload

---
+++ EXODB2/EXODB211/Source/make.bat
@@ -0,0 +1,25 @@
+tpc.exe -b diagram.pas -ue:\pascal\turbo7
+pause
+rename diagram.exe diagram.dat
+tpc.exe -b distri.pas -ue:\pascal\turbo7
+pause
+rename distri.exe distri.dat
+tpc.exe -b export.pas -ue:\pascal\turbo7
+pause
+rename export.exe export.dat
+tpc.exe -b hrd.pas -ue:\pascal\turbo7
+pause
+rename hrd.exe hrd.dat
+tpc.exe -b list.pas -ue:\pascal\turbo7
+pause
+rename list.exe list.dat
+tpc.exe -b masscomp.pas -ue:\pascal\turbo7
+pause
+rename masscomp.exe masscomp.dat
+tpc.exe -b orbit.pas -ue:\pascal\turbo7
+pause
+rename orbit.exe orbit.dat
+tpc.exe -b starmap.pas -ue:\pascal\turbo7
+pause
+rename starmap.exe starmap.dat
+tpc.exe -b exodb211.pas -ue:\pascal\turbo7
